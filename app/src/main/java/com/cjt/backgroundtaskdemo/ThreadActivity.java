374618896bac6fc9ef3992663013a7e85dfd0b57
bw123456 <1612941156@qq.com>
2024-07-01 15:03:07 +0800

    create

---
+++ app/src/main/java/com/cjt/backgroundtaskdemo/ThreadActivity.java
@@ -0,0 +1,14 @@
+package com.cjt.backgroundtaskdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThreadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_thread);
+    }
+}
